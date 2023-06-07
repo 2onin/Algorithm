fa0db81634c8a13e1d7e280037af89a82645870c
2onin <92674967+2onin@users.noreply.github.com>
2023-06-07 19:10:45 +0900

    [Silver V] Title: 배열 합치기, Time: 1656 ms, Memory: 354792 KB -BaekjoonHub

---
+++ 백준/Silver/11728. 배열 합치기/배열 합치기.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        List<Integer> A = new ArrayList<>();
+        List<Integer> B = new ArrayList<>();
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            A.add(Integer.parseInt(st.nextToken()));
+        }
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < M; i++) {
+            B.add(Integer.parseInt(st.nextToken()));
+
+        }
+        A.addAll(B);
+        Collections.sort(A);
+        for (int i:
+             A) {
+            sb.append(i).append(" ");
+        }
+        System.out.println(sb);
+    }
+}
