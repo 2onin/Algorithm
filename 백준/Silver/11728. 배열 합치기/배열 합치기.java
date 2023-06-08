86f4198e5eec36ffc5fbec49f06aebec7090d385
2onin <92674967+2onin@users.noreply.github.com>
2023-06-08 19:45:57 +0900

    [Silver V] Title: 배열 합치기, Time: 1500 ms, Memory: 301016 KB -BaekjoonHub

--- 백준/Silver/11728. 배열 합치기/배열 합치기.java
+++ 백준/Silver/11728. 배열 합치기/배열 합치기.java
@@ -1,35 +1,46 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+        // 포인터 두개를 지정, 배열 첫번째 위치 끼리 비교, 작은 쪽의 배열을 새로운 배열에 넣어주고 index값 증가
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
         StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
 
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
 
-        List<Integer> A = new ArrayList<>();
-        List<Integer> B = new ArrayList<>();
+        int A[] = new int[N];
+        int B[] = new int[M];
 
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
-            A.add(Integer.parseInt(st.nextToken()));
+            A[i] = Integer.parseInt(st.nextToken());
         }
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < M; i++) {
-            B.add(Integer.parseInt(st.nextToken()));
+            B[i] = Integer.parseInt(st.nextToken());
+        }
 
+        int APointer = 0, BPointer = 0;
+
+        while (APointer < N && BPointer < M){
+            if(A[APointer] < B[BPointer]){
+                sb.append(A[APointer++]).append(" ");
+            } else {
+                sb.append(B[BPointer++]).append(" ");
+            }
+        } //while 끝
+        while(APointer < N){
+            sb.append(A[APointer++]).append(" ");
         }
-        A.addAll(B);
-        Collections.sort(A);
-        for (int i:
-             A) {
-            sb.append(i).append(" ");
+        while(BPointer < M){
+            sb.append(B[BPointer++]).append(" ");
         }
+        
         System.out.println(sb);
     }
 }
